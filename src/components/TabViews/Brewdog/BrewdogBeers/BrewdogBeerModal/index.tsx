09f834491458a7ed5bd5fa0c153a901c08e40f04
Vlad <vladimir.strukelj@gmail.com>
2020-05-06 20:12:50 +0100

    more on modal

--- src/components/TabViews/Brewdog/BrewdogBeers/BrewdogBeerModal/index.tsx
+++ src/components/TabViews/Brewdog/BrewdogBeers/BrewdogBeerModal/index.tsx
@@ -1,3 +1,28 @@
 import * as React from 'react';
 import './brewdogbeer.css';
+import { Beer } from '../../../../../data/beers/types';
 
+interface BrewdogBeerModalProps {
+    beer: Beer;
+}
+
+const BrewdogBeerModal = ({beer}: BrewdogBeerModalProps) => {
+
+    return (
+        <div className="beer-modal">
+            <div className="beer-modal--wrapper">
+                <div className="beer-modal--wrapper-button-container">
+
+                </div>
+                <div className="beer-modal--wrapper-info-container">
+                    <div className="beer-modal--wrapper-info-container--left-child">
+
+                    </div>
+                    <div className="beer-modal--wrapper-info-container--right-child">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
