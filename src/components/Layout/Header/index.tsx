b96b05861de0dbd73e6790585d97b39cdfa170b3
Vlad <vladimir.strukelj@gmail.com>
2020-05-06 10:17:38 +0100

    more styling to header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -4,11 +4,14 @@ function Header() {
 
     return (
         <div className="header">
+            <div className="header--title">
+                <h1>Demo App</h1>
+            </div>
             <nav className="header--nav">
-                <div className="header--nav--item">icon food</div>
-                <div className="header--nav--item">icon all</div>
-                <div className="header--nav--item">icon settings</div>
-                <div className="header--nav--item">icon search</div>
+                <div className="header--nav--item"><h3>all</h3></div>
+                <div className="header--nav--item"><h3>food</h3></div>
+                <div className="header--nav--item"><h3>settings</h3></div>
+                <div className="header--nav--item"><h3>search</h3></div>
                 <div className="header--nav--animation header--nav--start-home" />
             </nav>
         </div>
