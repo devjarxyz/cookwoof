772273c98dde201b8519668e5a4ff0b5e17ec312
Vlad <vladimir.strukelj@gmail.com>
2020-05-07 10:01:04 +0100

    beer modal

--- src/components/TabViews/Brewdog/BrewdogBeers/BrewdogBeerModal/index.tsx
+++ src/components/TabViews/Brewdog/BrewdogBeers/BrewdogBeerModal/index.tsx
@@ -11,15 +11,23 @@ const BrewdogBeerModal = ({beer}: BrewdogBeerModalProps) => {
     return (
         <div className="beer-modal">
             <div className="beer-modal--wrapper">
-                <div className="beer-modal--wrapper-button-container">
-
+                <div className="beer-modal--wrapper-close-button-container">
+                    <button onClick={() => console.log('cloising')}>Close</button>
                 </div>
                 <div className="beer-modal--wrapper-info-container">
                     <div className="beer-modal--wrapper-info-container--left-child">
-
+                        <h4>{beer.name}</h4>
+                        <p>{beer.tagline}</p>
+                        <p>{beer.description}</p>
+                        <p>{beer.food_pairing}</p>
                     </div>
                     <div className="beer-modal--wrapper-info-container--right-child">
+                        <div className="beer-modal--wrapper-info-container--right-child--image">
 
+                        </div>
+                        <div className="beer-modal--wrapper-info-container--right-child--button">
+
+                        </div>
                     </div>
                 </div>
             </div>
