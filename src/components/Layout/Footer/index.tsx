36adade68441946693fd710609f3628b4af2f2df
Vlad <vladimir.strukelj@gmail.com>
2020-05-07 14:12:31 +0100

    footer

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -3,26 +3,50 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 function Footer () {
-    const initialFooterSizeState = {fullFooter: false, halfFooter: false };
+    const initialFooterSizeState = {halfFooter: false, fullFooter: false };
     const footerRef = React.useRef<HTMLDivElement>(null);
     const [ footerShow, setFooterShow ] = React.useState(initialFooterSizeState);
+    const [ startPos, setStartPos ] = React.useState(0);
    
+  
+    
+    const minDistance = 20;
+
     React.useEffect(() => {
-        if(footerShow.halfFooter && !footerShow.fullFooter) {
-            footerRef.current?.classList.add('half');
+        if(footerShow.halfFooter) {
             footerRef.current?.classList.remove('full');
-        } else if (!footerShow.halfFooter && footerShow.fullFooter) {
+            footerRef.current?.classList.add('half');            
+        } else if (footerShow.fullFooter) {
             footerRef.current?.classList.remove('half');
             footerRef.current?.classList.add('full');
+        } else if(!footerShow.fullFooter && !footerShow.halfFooter) {
             footerRef.current?.classList.remove('half');
+            footerRef.current?.classList.remove('full');
         }
     }, [ footerShow ])
 
     const touchStartHandler = (event: React.TouchEvent<HTMLElement>) => {
         console.log('start', event.touches);
+        setStartPos(event.touches[0].clientY);
+
     }
 
     const touchEndHandler = (event: React.TouchEvent<HTMLElement>) => {
+        const touch = event.changedTouches[0];
+        const swipeUp = touch.clientY < startPos;
+        const absY = Math.abs(touch.clientY - startPos);
+        console.log(absY);
+        if(absY > minDistance) {
+           
+            if(swipeUp){
+                console.log('up')
+                setFooterShow({fullFooter: true, halfFooter: false});
+              
+            } else {
+                console.log('down   ')
+                setFooterShow({fullFooter: false, halfFooter: false});
+            }
+        }
         console.log('end', event.changedTouches);
     }
 
@@ -32,16 +56,18 @@ function Footer () {
 
     return(
         <section className="footer" ref={footerRef} 
-        onTouchStart={(e) => touchStartHandler(e)}
-        onTouchEnd={(e) => touchEndHandler(e)}
-        onTouchMove={(e) => touchMoveHandler(e)}>
+        >
             <div className="inner">
-                <header>
-                   <hr style={{width: '5%', margin: '2px auto'}} onClick={() => {console.log('hej fix me')}}/>
+                <header  onClick={() => { console.log('clicked'); setFooterShow({fullFooter: false, halfFooter: true });} }>
+                   <hr style={{width: '5%', margin: '2px auto'}}/>
                    <p><FontAwesomeIcon icon={faShoppingCart} /> Shopping Cart </p>
                 </header>
-                
-                <div className="copyright">
+                <div onTouchStart={(e) => touchStartHandler(e)}
+        onTouchEnd={(e) => touchEndHandler(e)}
+        onTouchMove={(e) => touchMoveHandler(e)}>
+                    some data bal blalb alblb ba
+                </div>
+                <div className="copyright" >
                     &copy; cookwoof. All rights reserved.
                 </div>
             </div>
